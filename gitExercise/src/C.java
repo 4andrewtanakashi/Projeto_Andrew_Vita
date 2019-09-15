22c8ac125ea68021ea8b4fdb283b714104294645
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2019-09-14 22:06:48 -0300

    :rocket: Criação da classe C

---
+++ gitExercise/src/C.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Andrew
+ */
+public class C {
+    int C1;
+    float C2;
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+    
+    public void MC2() {
+        System.out.println("MC2");
+    }    
+    
+}
